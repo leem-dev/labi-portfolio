de2a49f27116505921dc7aa64452352f7ff94b89
leem-dev <alabitseleem@gmail.com>
2024-02-18 19:58:12 +0100

    mobile responsiveness

--- src/component/header-component/header-component.jsx
+++ src/component/header-component/header-component.jsx
@@ -1,30 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ShowPage from "./showPage-component";
 
 import "./header-styles.css";
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <a href="/" className="header-link">
-        <p className="header-text">
-          la<span>bi</span>
-        </p>
-      </a>
+    <Fragment>
+      <div className="header-container">
+        <a href="/" className="header-link">
+          <p className="header-text">
+            la<span>bi</span>
+          </p>
+        </a>
 
-      <ul className="header-ul">
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#project">Project</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-      <ShowPage />
-    </div>
+        <ul className="header-ul">
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#project">Project</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        <ShowPage />
+      </div>
+    </Fragment>
   );
 };
 
--- src/component/main-component/about-main-component.jsx
+++ src/component/main-component/about-main-component.jsx
@@ -1,25 +1,62 @@
-import { ReactComponent as Arrowright } from "../../assests/arrow-right.svg";
+// import { ReactComponent as Arrowright } from "../../assests/arrow-right.svg";
+import { useState, useEffect } from "react";
+import React from "react";
 
 const AboutFolder = () => {
-  const svg = {
-    width: "150px",
-    height: "35px",
+  const aboutMe = [
+    {
+      id: 1,
+      content:
+        "Greetings, I'm Seleem Alabi, an enthusiastic software developer specializing in Front-End development. With a solid background of 2 years, I excel in crafting compelling website and mobile pages, constantly seeking fresh challenges and pioneering solutions. Proficient in JavaScript, ReactJS, Node, CSS, and HTML, I am committed to perpetual learning and advancement in the dynamic tech landscape.",
+    },
+    {
+      id: 2,
+      content:
+        "I hold a Bachelor's degree in Civil Engineering and have completed the Meta Front-End Developer Specialization from Meta. Beyond coding, I have a keen interest in exploring emerging technologies and am enthusiastic about collaborating with fellow enthusiasts to contribute to meaningful projects. Please take a moment to browse through my portfolio, and don't hesitate to reach out!",
+    },
+  ];
+
+  const [showFirst, setShowFirst] = useState(true);
+  const [showSecond, setShowSecond] = useState(false);
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer((prevTimer) => prevTimer + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleMouseEnter = () => {
+    setTimer(0); // Reset the timer
+    setShowFirst(false);
+    setShowSecond(true);
   };
 
-  const aboutMe = [
-    "Greetings, I'm Seleem Alabi, an enthusiastic software developer specializing in Front-End development. With a solid background of 2 years, I excel in crafting compelling website and mobile pages, constantly seeking fresh challenges and pioneering solutions. Proficient in JavaScript, ReactJS, Node, CSS, and HTML, I am committed to perpetual learning and advancement in the dynamic tech landscape.",
-    "I hold a Bachelor's degree in Civil Engineering and have completed the Meta Front-End Developer Specialization from Meta. Beyond coding, I have a keen interest in exploring emerging technologies and am enthusiastic about collaborating with fellow enthusiasts to contribute to meaningful projects. Please take a moment to browse through my portfolio, and don't hesitate to reach out!",
-  ];
+  const handleMouseLeave = () => {
+    setTimer(0); // Reset the timer
+    setShowFirst(true);
+    setShowSecond(false);
+  };
+
   return (
-    <div className="about-container">
+    <div
+      className="about-container"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <h3 id="about">About Me</h3>
       <div className="about-details">
-        {aboutMe.map((about, index) => (
-          <div key={index} className="about-parag">
-            <Arrowright style={svg} className="svg" />
-            <p>{about}</p>
-          </div>
-        ))}
+        <div className={`paragraph ${showFirst ? "show" : "hide"}`}>
+          {aboutMe.map((paragraph) => (
+            <p key={paragraph.id}>{paragraph.content}</p>
+          ))}
+        </div>
+        <div className={`paragraph ${showSecond ? "show" : "hide"}`}>
+          {aboutMe.map((paragraph) => (
+            <p key={paragraph.id}>{paragraph.content}</p>
+          ))}
+        </div>
       </div>
     </div>
   );
