be4b107c072cba589236b559a60deebb14329d5a
leem-dev <alabitseleem@gmail.com>
2024-02-20 23:13:36 +0100

    responsiveness

--- src/component/main-component/project-section/project-main-component.jsx
+++ src/component/main-component/project-section/project-main-component.jsx
@@ -4,16 +4,19 @@ import Project from "./project-showmore";
 const ProjectFolder = ({ projects }) => {
   return (
     <div id="project" className="project-container">
-      {projects.map((project) => (
-        <Project
-          key={project.id}
-          img={project.img}
-          title={project.title}
-          description={project.description}
-          link={project.link}
-          tool={project.tool}
-        />
-      ))}
+      <h3>Project</h3>
+      <div>
+        {projects.map((project) => (
+          <Project
+            key={project.id}
+            img={project.img}
+            title={project.title}
+            description={project.description}
+            link={project.link}
+            tool={project.tool}
+          />
+        ))}
+      </div>
     </div>
   );
 };
