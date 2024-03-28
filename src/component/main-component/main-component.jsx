023037bd4903372e8d4ef2711e533af91d5991d4
leem-dev <alabitseleem@gmail.com>
2024-03-28 22:19:35 +0100

    add project

--- src/component/main-component/main-component.jsx
+++ src/component/main-component/main-component.jsx
@@ -38,10 +38,10 @@ const Main = () => {
     {
       id: 4,
       title: "Password Generator",
-
+      img: require("../../assests/"),
       description:
         "This project will allow users to generate random passwords with customizable length and options for including uppercase letters, lowercase letters, numbers, and special characters.",
-
+      link: "https://password-generator-lemon-two.vercel.app/",
       tool: "ReactJS, NPM, CSS",
     },
     {
